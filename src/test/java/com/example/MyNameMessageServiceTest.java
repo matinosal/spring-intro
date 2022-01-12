e9d572cdba19b4f49a5a93374964d5d3d7ea4f45
Mateusz Nosal <mateusz.nosal@vp.pl>
2022-01-12 15:41:25 +0100

    feature // zad 1

--- src/test/java/com/example/MyNameMessageServiceTest.java
+++ src/test/java/com/example/MyNameMessageServiceTest.java
@@ -1,14 +1,15 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@Component("messageService")
 class MyNameMessageServiceTest {
 
     @Test
     public void shouldReturnMyNameAsMessage() {
-        String expectedMessage = "<your name>";
+        String expectedMessage = "Mateusz Nosal";
         MyNameMessageService myNameMessageService = new MyNameMessageService();
 
         assertEquals(expectedMessage, myNameMessageService.getMessage());
